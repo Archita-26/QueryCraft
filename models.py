385b641577cd5b856c6bc8f7bb0768f1e5ccf316
Archita Temre <architatemre2610@gmail.com>
2026-09-16 23:36:04 +0530

    Add database models

---
+++ models.py
@@ -0,0 +1,33 @@
+from sqlalchemy import Column, Integer, String, DateTime
+from sqlalchemy.orm import declarative_base
+from datetime import datetime
+
+Base = declarative_base()
+
+class User(Base):
+    __tablename__ = "users"
+
+    id = Column(Integer, primary_key=True, index=True)
+    name = Column(String)
+    email = Column(String, unique=True)
+    password = Column(String)
+    created_at = Column(DateTime, default=datetime.utcnow)
+    
+    class Question(Base):
+    __tablename__ = "questions"
+
+    id = Column(Integer, primary_key=True, index=True)
+    title = Column(String)
+    description = Column(String)
+    difficulty = Column(String)
+    topic = Column(String)
+    
+    class Submission(Base):
+    __tablename__ = "submissions"
+
+    id = Column(Integer, primary_key=True, index=True)
+    user_id = Column(Integer)
+    question_id = Column(Integer)
+    submitted_query = Column(String)
+    is_correct = Column(Integer)
+    submitted_at = Column(DateTime, default=datetime.utcnow)
